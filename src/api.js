cb942ec1fd0876e0fb529664538b0f544c107e55
LAPTOP-L30HHH1L\yumna <yumna.wiratmaja@gmail.com>
2023-10-06 16:59:01 +0700

    update sensitive information

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ const options = {
     type: 'home'
   },
   headers: {
-    'X-RapidAPI-Key': '64946c5f2dmsh748e5c4b627803ap1886a4jsn0338531dadc3',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'football-web-pages1.p.rapidapi.com'
   }
 };
